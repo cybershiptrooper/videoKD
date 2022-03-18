722abae1d660fe3b01c7e4ed784cf50d202e56bf
cybershiptrooper <cybershiptrooper@gmail.com>
2022-03-18 22:16:01 +0530

    add files

--- load_kinetics.py
+++ load_kinetics.py
@@ -5,6 +5,16 @@ from torch.utils.data import Dataset
 from pytorchvideo.data.encoded_video import EncodedVideo
 from typing import Any, Callable, cast, Dict, List, Optional, Tuple
 
+def has_file_allowed_extension(filename: str, extensions: Union[str, Tuple[str, ...]]) -> bool:
+    """Checks if a file is an allowed extension.
+    Args:
+        filename (string): path to a file
+        extensions (tuple of strings): extensions to consider (lowercase)
+    Returns:
+        bool: True if the filename ends with one of given extensions
+    """
+    return filename.lower().endswith(extensions if isinstance(extensions, str) else tuple(extensions))
+
 class Kinetics(Dataset):
     """Face Landmarks dataset."""
 
