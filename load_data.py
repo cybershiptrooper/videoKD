bf0f4478733167d7b630115d86e4336a800ded0c
cybershiptrooper <cybershiptrooper@gmail.com>
2022-03-19 00:04:29 +0530

    add files

--- load_data.py
+++ load_data.py
@@ -26,36 +26,36 @@ def get_testloader(root, num_classes, batch_size, pin_memory=True):
 
 #Normalise mean = [0.485, 0.456, 0.406] and std = [0.229, 0.224, 0.225]
 
-train_pipeline = [
-    dict(type='DecordInit'),
-    dict(type='SampleFrames', clip_len=32, frame_interval=2, num_clips=1),
-    dict(type='DecordDecode'),
-    dict(type='Resize', scale=(-1, 256)),
-    dict(type='RandomResizedCrop'),
-    dict(type='Resize', scale=(224, 224), keep_ratio=False),
-    dict(type='Flip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='FormatShape', input_format='NCTHW'),
-    dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
-    dict(type='ToTensor', keys=['imgs', 'label'])
-]
-val_pipeline = [
-    dict(type='DecordInit'),
-    dict(
-        type='SampleFrames',
-        clip_len=32,
-        frame_interval=2,
-        num_clips=1,
-        test_mode=True),
-    dict(type='DecordDecode'),
-    dict(type='Resize', scale=(-1, 256)),
-    dict(type='CenterCrop', crop_size=224),
-    dict(type='Flip', flip_ratio=0),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='FormatShape', input_format='NCTHW'),
-    dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
-    dict(type='ToTensor', keys=['imgs'])
-]
+# train_pipeline = [
+#     dict(type='DecordInit'),
+#     dict(type='SampleFrames', clip_len=32, frame_interval=2, num_clips=1),
+#     dict(type='DecordDecode'),
+#     dict(type='Resize', scale=(-1, 256)),
+#     dict(type='RandomResizedCrop'),
+#     dict(type='Resize', scale=(224, 224), keep_ratio=False),
+#     dict(type='Flip', flip_ratio=0.5),
+#     dict(type='Normalize', **img_norm_cfg),
+#     dict(type='FormatShape', input_format='NCTHW'),
+#     dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+#     dict(type='ToTensor', keys=['imgs', 'label'])
+# ]
+# val_pipeline = [
+#     dict(type='DecordInit'),
+#     dict(
+#         type='SampleFrames',
+#         clip_len=32,
+#         frame_interval=2,
+#         num_clips=1,
+#         test_mode=True),
+#     dict(type='DecordDecode'),
+#     dict(type='Resize', scale=(-1, 256)),
+#     dict(type='CenterCrop', crop_size=224),
+#     dict(type='Flip', flip_ratio=0),
+#     dict(type='Normalize', **img_norm_cfg),
+#     dict(type='FormatShape', input_format='NCTHW'),
+#     dict(type='Collect', keys=['imgs', 'label'], meta_keys=[]),
+#     dict(type='ToTensor', keys=['imgs'])
+# ]
 
 if __name__ == "__main__":
 
